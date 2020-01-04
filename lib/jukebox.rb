4ed563baec6f41e7b5719e40c0e54a660c94d389
Theresa <thetheresagarcia@gmail.com>
2020-01-04 02:42:24 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -37,7 +37,7 @@ def run(songs)
   while true do
     puts "Please enter a command:"
     response = gets.chomp
-    case response
+    case response.downcase
     when "exit"
       exit_jukebox
       break
